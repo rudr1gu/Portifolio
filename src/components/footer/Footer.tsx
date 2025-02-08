bffc4da74f4b30d80273574c4f4d13eab8a462ae
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-08 14:11:48 -0300

    :sparkles: feat: Enhance Footer component with smooth scrolling navigation and icon integration

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,16 +1,45 @@
-import { FaInstagram, FaGithub, FaLinkedin, FaYoutube } from "react-icons/fa";
+import { FaInstagram, FaGithub, FaLinkedin, FaYoutube, FaHome, FaUser, FaTools, FaProjectDiagram, FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
+
+  const scrollToSection = (id: string) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+
   return (
     <footer className="bg-gray-900 py-16 text-white">
       <div className="container mx-auto text-center px-4">
         <nav className="mb-6">
           <ul className="flex flex-wrap justify-center gap-6 text-sm font-medium">
-            <li><a href="#" className="hover:text-blue-500 transition">Inicio</a></li>
-            <li><a href="#" className="hover:text-blue-500 transition">Sobre</a></li>
-            <li><a href="#" className="hover:text-blue-500 transition">Skills</a></li>
-            <li><a href="#" className="hover:text-blue-500 transition">Projetos</a></li>
-            <li><a href="#" className="hover:text-blue-500 transition">Contato</a></li>
+            <li>
+              <a onClick={() => scrollToSection("home")} className="flex gap-2 cursor-pointer hover:text-blue-500 transition">
+                <FaHome /> Início
+              </a>
+            </li>
+            <li>
+              <a onClick={() => scrollToSection("about")} className="flex gap-2 cursor-pointer hover:text-blue-500 transition">
+                 <FaUser /> Sobre
+              </a>
+            </li>
+            <li>
+              <a onClick={() => scrollToSection("skill")} className="flex gap-2 cursor-pointer hover:text-blue-500 transition">
+                <FaTools /> Skills
+              </a>
+            </li>
+            <li>
+
+              <a onClick={() => scrollToSection("project")} className="flex gap-2 cursor-pointer hover:text-blue-500 transition">
+               <FaProjectDiagram /> Projetos
+              </a>
+            </li>
+            <li>
+              <a onClick={() => scrollToSection("contact")} className="flex gap-2 cursor-pointer hover:text-blue-500 transition">
+                <FaEnvelope /> Contato
+              </a>
+            </li>
           </ul>
         </nav>
         <p className="text-xs uppercase font-semibold tracking-wide mb-6">© 2025 Rudr1gu. All rights reserved</p>
