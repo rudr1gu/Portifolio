897b5f214f5543def321a6fc6d8a6e4dc0924027
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-09 12:46:42 -0300

    :sparkles: feat: Implement intersection observer for Project component animations

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,19 +1,38 @@
+import { useEffect, useRef, useState } from "react";
 import ProjectCard from "./projectcard/ProjectCard";
 import projects from "./Projects";
 import { motion } from "framer-motion";
 
 const Project = () => {
+    const [isVisible, setIsVisible] = useState(false);
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entries]) => setIsVisible(entries.isIntersecting),
+            {threshold: 0.4}
+        );
+
+        if (ref.current){
+            observer.observe(ref.current);
+        }
+
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
+
     return (
-        <section className="py-20 px-4 bg-gray-200 dark:bg-gray-900">
+        <section ref={ref} id="project" className="py-20 px-4 bg-gray-200 dark:bg-gray-900">
             <div className="max-w-5xl mx-auto text-center">
                 <h2 className="text-4xl font-bold text-gray-800 dark:text-white mb-12">Projetos em Destaque</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                     {projects.map((project, index) => (
                         <motion.div
                         key={index}
-                        initial={{ opacity: 0, y: 50 }} // Começa invisível e deslocado para baixo
-                        animate={{ opacity: 1, y: 0 }} // Fica visível e sobe suavemente
-                        transition={{ duration: 0.5, delay: index * 0.2 }} // Atraso para efeito cascata
+                        initial={{ opacity: 0, y: 50 }}
+                        animate={isVisible ? { opacity: 1, y: 0 }: {}}
+                        transition={{ duration: 0.5, delay: index * 0.2 }}
                         >
                         <ProjectCard
                             key={index}
