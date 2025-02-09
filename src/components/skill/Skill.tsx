2611570958cffa74815a86677a164db0b73dee67
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-09 13:12:11 -0300

    :sparkles: feat: Implement intersection observer for Skill component to trigger animations on visibility

--- src/components/skill/Skill.tsx
+++ src/components/skill/Skill.tsx
@@ -1,10 +1,29 @@
 import { motion } from "framer-motion";
 import SkillCard from "./skillcard/SkillCard";
 import skillsData from "./skillsData";
+import { useEffect, useRef, useState } from "react";
 
 const Skill = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entries]) => setIsVisible(entries.isIntersecting),
+      { threshold: 0.3 }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
-    <section className="py-20 px-4 bg-gray-200 dark:bg-gray-900">
+    <section ref={ref} id="skill" className="py-20 px-4 bg-gray-200 dark:bg-gray-900">
       <div className="max-w-5xl mx-auto text-center">
         <h2 className="text-4xl font-bold text-gray-800 dark:text-white mb-12">
           Habilidades Técnicas
@@ -14,9 +33,8 @@ const Skill = () => {
             <motion.div
               key={index}
               initial={{ opacity: 0, x: -100 }}
-              whileInView={{ opacity: 1, x: 0 }}
+              animate={isVisible ? { opacity: 1, x: 0 } : {}}
               transition={{ duration: 0.6, ease: "easeOut", delay: index * 0.2 }}
-              viewport={{ once: true }}
             >
               <SkillCard icon={skill.icon} title={skill.title} skills={skill.skills} />
             </motion.div>
